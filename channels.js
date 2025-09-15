8b33f9e98817a7c34336dd3420e5a650cb4d8ba3
Fulltvtek <166868431+Fulltvtek@users.noreply.github.com>
2025-09-15 11:07:04 -0300

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,12 @@
+// Somente a lista de canais (global)
+window.CHANNELS = [
+  { id: 1, number: 1, name: "Globo SP", category: "Abertos", quality: "HD", logoUrl: "https://reidoscanais.life/img/tvglobo.png", live: true, streamUrl: "https://reidoscanais.life/embed/?id=globosp-globosaopaulo" },
+  { id: 2, number: 2, name: "Globo RJ", category: "Abertos", quality: "HD", logoUrl: "https://reidoscanais.life/img/tvglobo.png", live: true, streamUrl: "https://reidoscanais.life/embed/?id=globorj-globorio" },
+  { id: 3, number: 3, name: "SBT",      category: "Abertos", quality: "HD", logoUrl: "https://reidoscanais.life/img/sbt.png", live: true, streamUrl: "https://reidoscanais.life/embed/?id=sbt" },
+  { id: 4, number: 4, name: "BAND",     category: "Abertos", quality: "HD", logoUrl: "https://reidoscanais.life/img/band.png", live: true, streamUrl: "https://reidoscanais.life/embed/?id=band" },
+  { id: 5, number: 5, name: "Record",   category: "Abertos", quality: "HD", logoUrl: "https://reidoscanais.life/img/record.png", live: true, streamUrl: "https://reidoscanais.life/embed/?id=record" },
+
+  { id: 6,  number: 6,  name: "1 - Lounge",             category: "Destaque", quality: "HD", logoUrl: "https://i.ibb.co/N6TQfNbf/Estrela-da-Casa-2-Logo-1.jpg", live: true, streamUrl: "https://reidoscanais.life/embed/?id=estreladacasa" },
+  { id: 7,  number: 7,  name: "2- Dormitorio",          category: "Destaque", quality: "HD", logoUrl: "https://i.ibb.co/N6TQfNbf/Estrela-da-Casa-2-Logo-1.jpg", live: true, streamUrl: "https://reidoscanais.life/embed/?id=estreladacasa2" },
+  { id: 8,  number: 8,  name: "3- Refeitorio",          category: "Destaque", quality: "HD", logoUrl: "https://i.ibb.co/N6TQfNbf/Estrela-da-Casa-2-Logo-1.jpg", live: true, streamUrl: "https://reidoscanais.life/embed/?id=estreladacasa3" },
+  { id: 9,  number: 9,  name: "4 - Sala de Composição", category: "Destaque", quality: "HD", logoUrl: "https://i.ibb.co/N6TQfNbf/Estrela-da-Casa-2-Logo-1.jpg", live: true, streamUrl: "https://reidos
